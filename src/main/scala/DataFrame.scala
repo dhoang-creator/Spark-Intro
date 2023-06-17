a96f84ae524fcad421df22080ac7d42a312fbe1c
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-06-17 13:39:57 +0200

    test

--- src/main/scala/DataFrame.scala
+++ src/main/scala/DataFrame.scala
@@ -1,7 +1,7 @@
 import Main.spark
 import org.apache.spark.sql.SparkSession
 
-// We need to revisit the DF taken from a url or from a 
+// We need to revisit the DF taken from a url or from a
 object DataFrame {
 
   // does the below statement not simply conflict with the df you've attempted to make in Main
